e6c52bce46e27295b8c49e1a8fdd5310b2c940d9
Martin Ghosal <martin.ghosal@sky.uk>
2021-12-28 13:20:36 +0000

    Start adding some perf tests with fixes

--- src/main/kotlin/Any.kt
+++ src/main/kotlin/Any.kt
@@ -29,16 +29,45 @@ fun Any?.setRoot(any: Any?) {
     (this as? any)?.setValue(null, null, any)
 }
 
-operator fun Any?.get(vararg route: Location): Any? = get(route.toList())
-operator fun Any?.set(vararg route: Location, newValue: Any?) = set(route.toList(), newValue)
+//Performance of the set/get below are questionable
+operator fun Any?.set(vararg any: Any, newValue: Any?) =
+    set(
+        route = any.toList().map {
+            when (it) {
+                is String -> Location(it)
+                is Int -> Location(it)
+                else -> throw IllegalArgumentException("Only strings and ints are supported as keys")
+            }
+        },
+        newValue = newValue
+    )
+operator fun Any?.get(vararg any: Any) =
+    get(
+        route = any.toList().map {
+            when (it) {
+                is String -> Location(it)
+                is Int -> Location(it)
+                else -> throw IllegalArgumentException("Only strings and ints are supported as keys")
+            }
+        }
+    )
+
+operator fun Any?.get(vararg route: Location): Any? = get(route = route.toList())
+operator fun Any?.set(vararg route: Location, newValue: Any?) = set(route = route.toList(), newValue = newValue)
 
 operator fun Any?.get(route: Route): Any? =
     when (route.size) {
+        0 -> this
         1 -> get(route.first())
         2 -> get(route.elementAt(0))[route.elementAt(1)]
         3 -> get(route.elementAt(0))[route.elementAt(1)][route.elementAt(2)]
         4 -> get(route.elementAt(0))[route.elementAt(1)][route.elementAt(2)][route.elementAt(3)]
-        else -> throw IllegalArgumentException("You went too deep")
+        5 -> get(route.elementAt(0))[route.elementAt(1)][route.elementAt(2)][route.elementAt(3)][route.elementAt(4)]
+        6 -> get(route.elementAt(0))[route.elementAt(1)][route.elementAt(2)][route.elementAt(3)][route.elementAt(4)][route.elementAt(5)]
+        7 -> get(route.elementAt(0))[route.elementAt(1)][route.elementAt(2)][route.elementAt(3)][route.elementAt(4)][route.elementAt(5)][route.elementAt(6)]
+        8 -> get(route.elementAt(0))[route.elementAt(1)][route.elementAt(2)][route.elementAt(3)][route.elementAt(4)][route.elementAt(5)][route.elementAt(6)][route.elementAt(7)]
+        9 -> get(route.elementAt(0))[route.elementAt(1)][route.elementAt(2)][route.elementAt(3)][route.elementAt(4)][route.elementAt(5)][route.elementAt(6)][route.elementAt(7)][route.elementAt(8)]
+        else -> get(route.elementAt(0))[route.elementAt(1)][route.elementAt(2)][route.elementAt(3)][route.elementAt(4)][route.elementAt(5)][route.elementAt(6)][route.elementAt(7)][route.elementAt(8)][route.drop(9)]
     }
 
 operator fun Any?.set(route: Route, newValue: Any?) {
@@ -57,8 +86,8 @@ operator fun Any?.set(fork: Location, newValue: Any?) =
         is Location.Index -> this[fork.index] = newValue
     }
 
-operator fun Any?.get(key: String): Any? = (
-        (this as? Map<String, Any>) ?: (this() as? Map<String, Any>))?.get(key)()
+operator fun Any?.get(key: String): Any? =
+    ((this as? Map<String, Any>) ?: (this() as? Map<String, Any>))?.getOrDefault(key, null).run { this() ?: this }
 
 operator fun Any?.set(key: String, newValue: Any?) {
     val map = (this() as? MutableMap<String, Any?>) ?: mutableMapOf()
@@ -69,11 +98,29 @@ operator fun Any?.set(key: String, newValue: Any?) {
     setRoot(if (map.isEmpty()) null else map)
 }
 
-//TODO the invoke on the this() is causing problems
 operator fun Any?.get(index: Int): Any? =
-    ((this as? Collection<*>) ?: (this() as? Collection<*>))?.elementAtOrNull(index)()
+    ((this as? Collection<*>) ?: (this() as? Collection<*>))?.elementAtOrNull(index)?.run { this() ?: this }
 
 //TODO port the rest
+
+//set {
+//    guard 0... ~= index else {
+//        return
+//    }
+//    var o = self as? [Any?] ?? []
+//    o.append(contentsOf: repeatElement(nil, count: max(0, index - o.endIndex + 1)))
+//    o[index] = newValue
+//    if o.last ?? nil == nil {
+//        guard let i = o.dropLast().lastIndex(where: { $0 != nil }).map({ $0 + 1 }) else {
+//        self = nil
+//        return
+//    }
+//        o.removeSubrange(i...)
+//    }
+//    self = o
+//}
+//
+
 operator fun Any?.set(index: Int, newValue: Any?) {
 // Unsure whats going on here
 //    guard 0... ~= index else {
@@ -110,10 +157,15 @@ internal fun Any?.delegateGet(key: String): Any? = (this() as? Map<String, Any>)
 
 internal fun Any?.delegateGet(route: Route): Any? =
     when (route.size) {
+        0 -> this
         1 -> delegateGet(route.first())
         2 -> delegateGet(route.elementAt(0)).delegateGet(route.elementAt(1))
         3 -> delegateGet(route.elementAt(0)).delegateGet(route.elementAt(1)).delegateGet(route.elementAt(2))
-        4 -> delegateGet(route.elementAt(0)).delegateGet(route.elementAt(1)).delegateGet(route.elementAt(2))
-            .delegateGet(route.elementAt(3))
-        else -> throw IllegalArgumentException("You went too deep")
+        4 -> delegateGet(route.elementAt(0)).delegateGet(route.elementAt(1)).delegateGet(route.elementAt(2)).delegateGet(route.elementAt(3))
+        5 -> delegateGet(route.elementAt(0)).delegateGet(route.elementAt(1)).delegateGet(route.elementAt(2)).delegateGet(route.elementAt(3)).delegateGet(route.elementAt(4))
+        6 -> delegateGet(route.elementAt(0)).delegateGet(route.elementAt(1)).delegateGet(route.elementAt(2)).delegateGet(route.elementAt(3)).delegateGet(route.elementAt(4)).delegateGet(route.elementAt(5))
+        7 -> delegateGet(route.elementAt(0)).delegateGet(route.elementAt(1)).delegateGet(route.elementAt(2)).delegateGet(route.elementAt(3)).delegateGet(route.elementAt(4)).delegateGet(route.elementAt(5)).delegateGet(route.elementAt(6))
+        8 -> delegateGet(route.elementAt(0)).delegateGet(route.elementAt(1)).delegateGet(route.elementAt(2)).delegateGet(route.elementAt(3)).delegateGet(route.elementAt(4)).delegateGet(route.elementAt(5)).delegateGet(route.elementAt(6)).delegateGet(route.elementAt(7))
+        9 -> delegateGet(route.elementAt(0)).delegateGet(route.elementAt(1)).delegateGet(route.elementAt(2)).delegateGet(route.elementAt(3)).delegateGet(route.elementAt(4)).delegateGet(route.elementAt(5)).delegateGet(route.elementAt(6)).delegateGet(route.elementAt(7)).delegateGet(route.elementAt(8))
+        else -> delegateGet(route.elementAt(0)).delegateGet(route.elementAt(1)).delegateGet(route.elementAt(2)).delegateGet(route.elementAt(3)).delegateGet(route.elementAt(4)).delegateGet(route.elementAt(5)).delegateGet(route.elementAt(6)).delegateGet(route.elementAt(7)).delegateGet(route.elementAt(8)).delegateGet(route.drop(9))
     }
--- src/test/kotlin/AnyTest.kt
+++ src/test/kotlin/AnyTest.kt
@@ -1,15 +1,22 @@
 package com.bskyb.skynamespace
 
+import Location
 import any
+import com.bskyb.skynamespace.utils.RandomRoutes
 import com.bskyb.skynamespace.utils.not
 import delegateGet
 import org.junit.jupiter.api.Test
 import get
 import invoke
 import set
+import kotlin.system.measureNanoTime
+import kotlin.system.measureTimeMillis
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
+@OptIn(ExperimentalTime::class)
 class AnyTest {
 
     @Test
@@ -31,46 +38,97 @@ class AnyTest {
         o["one"] = 1
         assertEquals(1, o["one"])
 
-        o[!"one", !2] = 2
-        assertEquals(2, o[!"one", !2])
+        o["one", 2] = 2
+        assertEquals(2, o["one", 2])
 
-        o[!"one", !3] = null
+        o["one", 3] = null
 
-        o[10] = 10
-        assertEquals(10, o[10])
-
-        assertEquals(listOf(null, null, 2), o[!"one"])
+        o["one", 10] = 10
+        assertEquals(10, o["one", 10])
     }
 
-//
-//    func test_subscript() throws {
-//
-//        var o: Any?
-//
-//        o = nil
-//        o = []
-//        o = [:]
-//
-//        o[] = "👋"
-//        try hope(o[]) == "👋"
-//
-//            o["one"] = 1
-//            try hope(o["one"]) == 1
-//
-//                o["one", 2] = 2
-//                try hope(o["one", 2]) == 2
-//
-//                    o["one", 3] = nil
-//                    try hope(o["one"]) == [nil, nil, 2] // did not append
-//                        o["one", 2] = ["three": 4]
-//                        try hope(o["one", 2, "three"]) == 4
-//                            try hope(o[\.["one"][2]["three"]]) == 4
-//
-//                                o["one", 2] = nil
-//                                hope.true(o["one"] == nil)
-//
-//                                o["one", "two"] = nil
-//                                hope.true(o[] == nil)
-//                            }
-//
+    @Test
+    fun swiftFunctionality() {
+        var o: Any? by any()
+
+        o = null
+        o = emptyList<Any?>()
+        o = emptyMap<Any?, Any?>()
+
+        o = "👋"
+        assertEquals("👋", o())
+
+        o["one"] = 1
+        assertEquals(1, o["one"])
+
+        o["one", 2] = 2
+        assertEquals(2, o["one", 2])
+
+        o["one", 3] = null
+        assertEquals(listOf(null, null, 2), o["one"])
+
+        o["one", 2] = mapOf("three" to 4)
+        assertEquals(4, o["one", 2, "three"])
+
+        o["one", 2] = null
+        assertNull(o["one"])
+
+        o["one", "two"] = "martin was here"
+        assertNull(o())
+    }
+
+    @Test
+    fun nestedKeys(){
+        val o: Any? by any()
+
+        val route = listOf("e", "a", "a", "e", "e").map { Location(it) }
+        o["e", "a"] = "✅"
+
+        println(o)
+        assertEquals("✅", o["e", "a"])
+    }
+
+    @Test
+    fun `any path performance`(){
+        val randomRoutes =
+            RandomRoutes(
+                keys = "abcde".map { it.toString() },
+                indices = listOf(1, 2, 3),
+                keyBias = 0.8f,
+                length = 5..20,
+                seed = 4
+            )
+
+        val routes = randomRoutes.generate(10000)
+
+        val time = measureTime {
+            routes.forEach {
+                get(it)
+            }
+        }
+        println(time)
+    }
+
+    @Test
+    fun `set performance`(){
+        val randomRoutes =
+            RandomRoutes(
+                keys = "abcde".map { it.toString() },
+                indices = listOf(1, 2, 3),
+                keyBias = 0.8f,
+                length = 5..20,
+                seed = 4
+            )
+
+        val routes = randomRoutes.generate(10000)
+
+        val o: Any by any()
+
+        val time = measureTime {
+            routes.forEach { route ->
+                o[route] = "✅"
+            }
+        }
+        println(time)
+    }
 }
